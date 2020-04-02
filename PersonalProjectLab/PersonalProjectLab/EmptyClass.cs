617af56e3f55ff2cceec0a5ecc4cf085c21fc20f
jhelzer3 <59981853+jhelzer3@users.noreply.github.com>
2020-04-02 15:09:34 -0500

    Update EmptyClass.cs

--- PersonalProjectLab/PersonalProjectLab/EmptyClass.cs
+++ PersonalProjectLab/PersonalProjectLab/EmptyClass.cs
@@ -1,44 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-<<<<<<< Updated upstream
+
 
 namespace SimonGame
-{ 
+{
 
     public class Button
     {
-        List<int> _InputList;
-        List<int> _OutputList;
+        
         int _buttonValue;
-        int _UserIn;
+        
 
 
         public Button(int buttonValue)
         {
             _buttonValue = buttonValue;
+            
 
         }
-      
+
         public void addToInputList(List<int> InputtyList, int UserIn)
         {
+            
             if (UserIn == _buttonValue)
             {
-                _InputList.Add(_buttonValue);
+                InputtyList.Add(_buttonValue);
             }
-            
-=======
-namespace SimonGame
-{
-    
-
-    public class Button
-    {
-      
-
-        public Button()
-        {
-
->>>>>>> Stashed changes
         }
     }
 }
+
+            
