e877c57997ee0404a03c59ef3a9557bc68c4d7be
jhelzer3 <59981853+jhelzer3@users.noreply.github.com>
2020-04-05 19:16:26 -0500

    Update UnitTest1.cs

--- PersonalProjectLab/PersonalProjectLabTests/UnitTest1.cs
+++ PersonalProjectLab/PersonalProjectLabTests/UnitTest1.cs
@@ -1,4 +1,10 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using SimonGame;
+using SimonGameClass;
+using System.Linq;
+
 
 namespace PersonalProjectLabTests
 {
@@ -6,8 +12,31 @@ namespace PersonalProjectLabTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ListComparison()
         {
+            //arrange
+            Button TestButton = new Button(5);
+            List<int> TestOutput = new List<int>();
+            List<int> TestInput = new List<int>();
+            int TestUserIn = 5;
+            //act
+            for (int i = 0; i <= 15; i++)
+            {
+                TestButton.addToInputList(TestInput, TestUserIn);
+                TestOutput.Add(5);
+
+            }
+            bool ListsAreSame;
+            if (TestOutput.SequenceEqual(TestInput))
+            {
+                ListsAreSame = true;
+            }
+            else
+            {
+                ListsAreSame = false;
+            }
+            //assert
+            Assert.AreEqual(ListsAreSame, true);
         }
     }
 }
