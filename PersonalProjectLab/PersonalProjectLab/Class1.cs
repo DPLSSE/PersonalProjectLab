c231c95f72736a07270a0528e2c20d9811c14e8b
jhelzer3 <59981853+jhelzer3@users.noreply.github.com>
2020-04-05 20:42:12 -0500

    Update Class1.cs

--- PersonalProjectLab/PersonalProjectLab/Class1.cs
+++ PersonalProjectLab/PersonalProjectLab/Class1.cs
@@ -6,9 +6,25 @@ using System.Threading;
 using System.Threading.Tasks;
 using SimonGameClass;
 
-namespace PersonalProjectLab
+namespace GamestatsClass
 {
-    class Class1
+    public class GameStats
     {
+        public GameStats()
+        {
+           
+        }
+        public void printStats(int finalNumber)
+        {
+            int numberOfPlays = finalNumber;
+            Console.WriteLine("Game over. Your final number of plays was " + numberOfPlays);
+            Thread.Sleep(2000);
+            Console.WriteLine("The list that killed you was " + numberOfPlays + " numbers long.");
+            Thread.Sleep(1500);
+            Console.WriteLine("Simon says,'Please play again!'");
+            Thread.Sleep(1500);
+            Console.WriteLine("Simon also says, 'Help! I'm trapped in a computer!'");
+
+        }
     }
 }
